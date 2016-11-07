44bf708cef5c60611ea9370e17756fae0afef8df
iugo <iugogogo@gmail.com>
2016-11-07 17:08:30 +0800

    调试

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -16,6 +16,7 @@ dd.config({
         'device.notification.prompt',
         'biz.chat.chooseConversation',
         'biz.contact.complexChoose',
+        'biz.contact.choose',
         'biz.ding.post']
 });
 
@@ -45,15 +46,35 @@ dd.ready(function() {
     //     }
     // });
 
-    dd.biz.contact.complexChoose({
-      startWithDepartmentId: 0, //-1表示从自己所在部门开始, 0表示从企业最上层开始，其他数字表示从该部门开始
-      selectedUsers: [], //预选用户
+    // 选择人和部门
+    // dd.biz.contact.complexChoose({
+    //   startWithDepartmentId: 0, //-1表示从自己所在部门开始, 0表示从企业最上层开始，其他数字表示从该部门开始
+    //   selectedUsers: [], //预选用户
+    //   corpId: _config.corpId, //企业id
+    //   onSuccess: function(data) {
+    //     sessionStorage.setItem('selectedPeople', JSON.stringify(data));
+    //     alert('已完成选人: ' + sessionStorage.getItem('selectedPeople'))
+
+    //     var theUsers = JSON.parse(sessionStorage.getItem('selectedPeople')).users.map(function (val) {
+    //         return val.emplId
+    //     })
+    //     alert(JSON.stringify(theUsers) || '已选中的用户出错')
+    //     sendMessage(theUsers, '您已经获得评分')
+    //   },
+    //   onFail : function(err) {
+    //     alert('出错了' + JSON.stringify(err))
+    //   }
+    // });
+
+    dd.biz.contact.choose({
+      startWithDepartmentId: 0, //-1表示打开的通讯录从自己所在部门开始展示, 0表示从企业最上层开始，(其他数字表示从该部门开始:暂时不支持)
+      multiple: true, //是否多选： true多选 false单选； 默认true
+      users: [], //默认选中的用户列表，userid；成功回调中应包含该信息
       corpId: _config.corpId, //企业id
       onSuccess: function(data) {
         sessionStorage.setItem('selectedPeople', JSON.stringify(data));
-        alert('已完成选人')
+        alert('已完成选人: ' + sessionStorage.getItem('selectedPeople'))
 
-        alert('查看 ' + sessionStorage.getItem('selectedPeople'))
         var theUsers = JSON.parse(sessionStorage.getItem('selectedPeople')).users.map(function (val) {
             return val.emplId
         })
