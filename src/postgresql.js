4232217901f58d8471a22c85178e8853344eafa6
iugo <iugogogo@gmail.com>
2016-11-17 15:21:20 +0800

    增加本地调试参数

--- src/postgresql.js
+++ src/postgresql.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const url = require('url');
 
-const params = url.parse(process.env.DATABASE_URL);
+const params = url.parse(process.env.DATABASE_URL || require('../env').postgreUrl);
 
 const auth = params.auth.split(':');
 
