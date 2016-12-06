3cdbab69a976cbc64ded565cee89e8f42c2bbe37
iugo <iugogogo@gmail.com>
2016-12-06 15:04:22 +0800

    新建评分任务(调试)

--- public/javascripts/mark.js
+++ public/javascripts/mark.js
@@ -11,10 +11,15 @@ var app = {
   _items: {},
 
   selectPlayers: function () {
+    choosePeople('players', function (data) {
+      this._players = data;
+      alert(this._players);
+    }.bind(this));
   },
 
   displayAllItems: function () {
     document.querySelector('.all-items').classList.remove('hidden');
+    scroll(0, 0);
   },
 
   hideAllItems: function () {
@@ -32,11 +37,52 @@ var app = {
       score: 0,
       markers: [],
     };
-    console.log('选择了: ' + this._allItems[id].name);
+
+    this._renderSelectItem(id, 'add');
+    this._renderItemDom(this._allItems[id]);
+    this.hideAllItems();
+  },
+
+  _renderSelectItem: function (id, change) {
+    var num = this._allItems.length - id;
+    var el = document.querySelector('.all-items li:nth-child(' + num + ')');
+    if (change === 'add') {
+      return el.classList.add('selected');
+    }
+    return el.classList.remove('selected');
   },
 
   _renderItemDom: function (data) {
-
+    // TODO: 将 label 和 input 进行关联,
+    //       https://www.w3.org/wiki/HTML/Elements/label
+    var el1 = document.createElement('fieldset');
+    var el11 = document.createElement('legend');
+    var el12 = document.createElement('label');
+    var el13 = document.createElement('input');
+    var el14 = document.createElement('div');
+    var el15 = document.createElement('label');
+    var el16 = document.createElement('ul');
+    var el17 = document.createElement('button');
+    el1.classList.add('left-space');
+    el1.id = 'item' + data.id;
+    el11.innerText = data.name;
+    el12.innerText = '总分值';
+    el13.setAttribute('type', 'text');
+    el13.setAttribute('name', 'score' + data.id);
+    el14.classList.add('bottom-space');
+    el15.innerText = '评分人';
+    el16.id = 'markers' + data.id;
+    el17.classList.add('pure-button');
+    el17.innerText = '选择评分人';
+    el17.setAttribute('onclick', 'choosePeople(\'markers\', ' + data.id + ')');
+    el1.appendChild(el11);
+    el1.appendChild(el12);
+    el1.appendChild(el13);
+    el1.appendChild(el14);
+    el1.appendChild(el15);
+    el1.appendChild(el16);
+    el1.appendChild(el17);
+    return document.querySelector('.pure-form > fieldset').appendChild(el1);
   },
 
   deleteItem: function (id) {
@@ -96,9 +142,26 @@ dd.config({
   timeStamp: _config.timeStamp,
   nonceStr: _config.nonceStr,
   signature: _config.signature,
-  jsApiList: ['runtime.permission',
-    'device.notification.alert'],
+  jsApiList: [
+    'runtime.permission',
+    'device.notification.alert',
+    'biz.contact',
+  ],
 });
+
+function choosePeople (key, fn) {
+  // TODO: 记录上次选中的人
+  dd.biz.contact.choose({
+    startWithDepartmentId: 0,
+    multiple: true,
+    users: [], // [String, ...]
+    corpId: _config.corpId,
+    onSuccess: fn,
+    onFail: function (err) {
+      alert('出错了' + JSON.stringify(err));
+    },
+  });
+}
 
 dd.ready(function () {
   dd.runtime.permission.requestAuthCode({
